367c9a4ad9b3b79c5d1eb99651785015911888a9
maranimitchelle@gmail.com <mitch@DESKTOP-PMCR53J.localdomain>
2025-07-02 18:58:00 +0300

    Event listeners

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+ const input = document.getElementById('button'); 
+    input.addEventListener("click", function () { //name of the event and a callback function to handle the event
+        alert("I was clicked!");
+    });
+    
 }
